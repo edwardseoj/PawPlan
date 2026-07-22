ca7fdd4b1011de2aab6d76f3e0c99361166d176c
Cereal <ciabattacereals@gmail.com>
2026-07-22 14:51:53 +0800

    gets reminders in db

--- views/petreminder.py
+++ views/petreminder.py
@@ -1,18 +1,46 @@
 import flet as ft
+import logging
+
+from google.cloud.firestore_v1 import FieldFilter
+from utility.firebase_setup import db
+
+logger = logging.getLogger(__name__)
 
 
 
-def main(page: ft.Page):
-    page.title = "PawPlan"
-    page.bgcolor = "#FFFFFF"
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.scroll = None
+def petreminder_view(page: ft.Page) -> ft.View:
 
     primary = "#0D6EFD"
     soft_border = "#DDE3EE"
     white = "#FFFFFF"
     white38 = "#FFFFFF66"
-#header
+
+
+    async def go_home(e):
+        logger.info("Home nav clicked")
+        try:
+            await page.push_route("/homepage")
+            logger.info("Route pushed")
+        except Exception as e:
+            logger.error(f"Error occurred: {e}")
+
+    async def go_calendar(e):
+        logger.info("Calendar nav clicked")
+        try:
+            await page.push_route("/calendar")
+            logger.info("Route pushed")
+        except Exception as e:
+            logger.error(f"Error occurred: {e}")
+
+    async def go_profile(e):
+        logger.info("Profile nav clicked")
+        try:
+            await page.push_route("/profile")
+            logger.info("Route pushed")
+        except Exception as e:
+            logger.error(f"Error occurred: {e}")
+
+    # header
     appbar = ft.Container(
         padding=ft.Padding.symmetric(horizontal=20, vertical=10),
         bgcolor="#FFFFFF",
@@ -24,7 +52,7 @@ def main(page: ft.Page):
         ),
     )
 
-#page title
+    # page title
     title = ft.Container(
         margin=ft.Margin.only(left=16, right=16, top=10),
         content=ft.Text(
@@ -51,12 +79,47 @@ def main(page: ft.Page):
         alarm_list_layout.controls.append(alarm_unit)
         page.update()
 
+
+    pet_name = "ben" # temporary, change later
+    current_user_id = page.auth.user["email"]  # Get the current user's email
+    reminder_ref = (
+        db.collection("users").document(current_user_id).collection("details").document("pets").collection("reminders").
+        where(filter=FieldFilter("pet", "==", pet_name))
+        .stream()
+    )
+    reminder_list = []
+    for reminder in reminder_ref:
+        data = reminder.to_dict()
+        reminder_list.append(data["name"])
+    print(reminder)
+
+    def create_reminder_control(reminder_name):
+        def delete_alarm(e):
+            alarm_list_layout.controls.remove(alarm_unit)
+            page.update()
+
+        alarm_unit = ft.Container(
+            margin=ft.Margin.only(left=20, right=20, top=10, bottom=10),
+            border=ft.Border.all(width=2.5, color=ft.Colors.GREY_400),
+            padding=15,
+            width=float("inf"),
+            content=ft.Text(reminder_name)
+        )
+        return alarm_unit
+
     alarm_list_layout = ft.Column(
         expand=True,
         scroll=ft.ScrollMode.ADAPTIVE,
+        controls=(
+            [create_reminder_control(reminder) for reminder in reminder_list]
+            if reminder_list
+            else [
+                ft.Text("No reminders")
+            ]
+        )
     )
 
-#reminder container
+    # reminder container
     alarm_cont = ft.Container(
         margin=ft.Margin.only(top=15, bottom=5),
         expand=True,
@@ -65,13 +128,13 @@ def main(page: ft.Page):
         content=alarm_list_layout  # Inject the list layout here
     )
 
-#addbutton
-    add_button =  ft.Container(
-        margin=ft.Margin.only(top=5,bottom=20),
+    # add button
+    add_button = ft.Container(
+        margin=ft.Margin.only(top=5, bottom=20),
         align=ft.Alignment.CENTER,
         content=ft.IconButton(
             icon=ft.Icons.ADD,
-            icon_color = "white",
+            icon_color="white",
             bgcolor="#0B4FB0",
             icon_size=24,
             tooltip="Add reminder",
@@ -79,7 +142,7 @@ def main(page: ft.Page):
         )
     )
 
-#navbar
+    # navbar
     bottom_nav = ft.Container(
         padding=ft.Padding.symmetric(horizontal=20, vertical=20),
         bgcolor="#0B4FB0",
@@ -88,42 +151,36 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.SPACE_AROUND,
             controls=[
                 ft.TextButton(
-                    "Home", style=ft.ButtonStyle(color=white), on_click=lambda e: None
+                    "Home", style=ft.ButtonStyle(color=white), on_click=go_home
                 ),
                 ft.TextButton(
                     "Calendar",
                     style=ft.ButtonStyle(color=white),
-                    on_click=lambda e: None,
+                    on_click=go_calendar,
                 ),
                 ft.TextButton(
                     "Profile",
                     style=ft.ButtonStyle(color=white),
-                    on_click=lambda e: None,
+                    on_click=go_profile,
                 ),
             ],
         ),
     )
 
-
-
-    page.add(
-        ft.Column(
-            spacing=0,
-            expand=True,
-            horizontal_alignment=ft.CrossAxisAlignment.STRETCH,
-            controls=[
-                appbar,
-                title,
-                alarm_cont,
-                add_button,
-                bottom_nav,
-            ],
-        )
+    return ft.View(
+        route="/petreminder",
+        controls=[
+            ft.Column(
+                spacing=0,
+                expand=True,
+                horizontal_alignment=ft.CrossAxisAlignment.STRETCH,
+                controls=[
+                    appbar,
+                    title,
+                    alarm_cont,
+                    add_button,
+                    bottom_nav,
+                ],
+            )
+        ],
     )
-
-    page.window.width= 430
-    page.window.height= 900
-
-
-if __name__ == "__main__":
-    ft.run(main)
