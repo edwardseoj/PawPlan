dc8be51c6743485688e6c072872a365290648e48
Cereal <ciabattacereals@gmail.com>
2026-07-31 18:04:44 +0800

    updated google setup

--- setup/google_dotenv_setup.py
+++ setup/google_dotenv_setup.py
@@ -6,15 +6,18 @@ load_dotenv()
 client_id = os.getenv("GOOGLE_CLIENT_ID")
 client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
 
-
-# error checking
 if not client_id or not client_secret:
     raise ValueError("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET in .env file")
 
+IS_WEB = os.environ.get("PORT") is not None
+REDIRECT_URL = (
+    os.getenv("OAUTH_REDIRECT_URL")
+    if IS_WEB
+    else "http://localhost:8550/oauth_callback"
+)
 
-# auth code
 provider = GoogleOAuthProvider(
     client_id=client_id,
     client_secret=client_secret,
-    redirect_url="http://localhost:8550/oauth_callback",
+    redirect_url=REDIRECT_URL,
 )
